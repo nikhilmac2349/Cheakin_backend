e3c6519f28a9baa07448f2401a7efaca568b7c13
AS-MAC-1277 <as-mac-1277@AS-MAC-1277.local>
2026-02-17 00:47:34 +0530

    vercel fix

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/professure", require("./routes/userRoutes"));
+app.use("/student" , require("./routes/studnetRoutes"))
+app.use("/actualcheakin" , require("./routes/actualCheakin"))
+app.use("/plannedcheakin" , require("./routes/plannedCheakin"))
+
+
+
+
+module.exports = app;
